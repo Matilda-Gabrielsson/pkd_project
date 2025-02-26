ec5bf78ec9ecfcc89da3e4756d1cd9db0a6d8c38
Engla Selander <engla.selander.9090@student.uu.se>
2025-02-26 11:00:00 +0100

    typ klar?

--- engla_project.ts
+++ engla_project.ts
@@ -8,47 +8,73 @@ const prompt: PromptSync.Prompt = PromptSync({ sigint: true });
  * */ 
 
 export function process_input() {
+    console.log("");
     console.log("Hello welcome to Groupsort!");
     console.log("");
     let option = ("");
     let f_index = 2;
     let index = 1;
     const person_array = [];
+
     while(option !== "STOP") {
         const name = prompt("Your name: ");
+        // om man inte vill lägga till någon?
         const friend = prompt("Your friends name: ");
+        let save_index = index;
+        let save_f_index = f_index;
+        let double_apply: boolean = false;
 
-     // om en person redan har tilldelats ett id av någon som önskat dem ska de få det istället
+        //om en person anmäler sig igen
         for(let i = 0; person_array[i] !== undefined; i = i + 1) {
-            if(person_array[i].friend_name === name) {
-                index = person_array[i].friend_id;
+            if(person_array[i].name === name) {
+                console.log("You have already applied for GroupSort")
+                double_apply = true;
+                
             }
-        }
-     // om en person man önkar redan finns som "huvud person" ska vännen få sitt tidigare id
-        for(let i = 0; person_array[i] !== undefined; i = i + 1) {
-            if(person_array[i].name === friend) {
-                f_index = person_array[i].id;
+        } 
+        if(double_apply === false) {
+            // om en person redan har tilldelats ett id av någon som önskat dem ska de få det istället
+            for(let i = 0; person_array[i] !== undefined; i = i + 1) {
+                if(person_array[i].friend_name === name) {
+                    index = person_array[i].friend_id;
+                }
             }
+            // om en person man önkar redan finns som "huvud person" ska vännen få sitt tidigare id
+            for(let i = 0; person_array[i] !== undefined; i = i + 1) {
+                if(person_array[i].name === friend) {
+                    f_index = person_array[i].id;
+                }
+            }
+            // om en persons vän redan är någon anans vän
+            for(let i = 0; person_array[i] !== undefined; i = i + 1) {
+                if(person_array[i].friend_name === friend) {
+                    f_index = person_array[i].friend_id;
+                }
+            }
+ 
+            const person: person = {
+                name: name!,
+                id: index, //ska få nåt annat om personen inte finns sen innan  
+                friend_name: friend!,
+                friend_id: f_index 
+            };
+            //lite osäker på om den här logiken kommer funka
+            index = save_index;
+            f_index = save_f_index;
+            index = index + 2;
+            f_index = f_index + 2;
+
+            person_array.push(person);
         }
-        
-        const person: person = {
-            name: name!,
-            id: index, //ska få nåt annat om personen inte finns sen innan  
-            friend_name: friend!,
-            friend_id: f_index 
-        };
-        //lite osäker på om den här logiken kommer funka
-        index = index + 2;
-        f_index = f_index + 2;
 
-        person_array.push(person); 
-
-        option = prompt("Write STOP to finish or press enter to continue: ")!;
+        console.log(" ")
+        option = prompt("Write STOP to finish or press ENTER to continue: ")!;
         console.log(" ")
         const big_option = option.toUpperCase();
         option = big_option;
     }
-    console.log(person_array);
+
+    //console.log(person_array);
     return person_array;
 }
 
