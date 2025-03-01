e953718f2b383e4ca29d35cd2649b4e36a8161b7
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-01 13:46:39 +0100

    Lagt till kommentarer

--- divide_if_too_many.ts
+++ divide_if_too_many.ts
@@ -1,18 +1,18 @@
 
 /**
  * 
- * @param groups 
- * @param number_of_groups 
- * @returns 
+ * @param groups grupperna
+ * @param number_of_groups antal grupper det får vara
+ * @param max_group_size maxantal i en grupp
+ * @returns rätt antal grupper där alltså folk har shiftats runt
  */
-
 export function divide_if_too_many(groups: number[][], number_of_groups: number, max_group_size: number): number[][] {
 
     if(groups.length > number_of_groups) {
         const find_small = find_smallest(groups);
         const smallest_group = find_small.group; //the smallest group
         const smallest_group_index: number = find_small.index; //the index of the smallest group
-        const groups_new = []
+        const groups_new: number[][] = [];
 
         for(let i = smallest_group_index; i < groups.length; i = i + 1) { //put the groups in a new array shifting them back
             groups_new[i] = groups[i + 1];
@@ -20,6 +20,8 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
 
         for(let i = 0; i < groups_new.length; i = i + 1) {
             if (groups_new[i].length + smallest_group.length <= max_group_size) {
+                //Om man kan lägga den minsta gruppen i en grupp utan att den blir för stor görs det
+                //Tänker typ med grupper på en person känns det rimligt
                 groups_new[i] = [...groups[i], ...smallest_group];
                 break;
             }   
@@ -28,12 +30,12 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
     } else {
         return groups;
     }
-
-
-
 }
 
-
+/**
+ * @param groups array av grupper
+ * @returns ett par(obs inte par men ja) av ett index och en grupp, den första minsta gruppen av grupperna
+ */
 export function find_smallest(groups: number[][]) {
     let current_small = groups[0].length;
     let current_small_index: number = 0;
@@ -46,10 +48,3 @@ export function find_smallest(groups: number[][]) {
     }
     return {index: current_small_index, group: groups[current_small_index]};
 }
-
-/**
- * ifall man får in för många grupper, vilken borde man dela på? den minsta?
- * find the smallest group and divide it?
- * 
- * 
- */
