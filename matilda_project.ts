fc2c93feb854b96cf7cfbc948503b363872261c2
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-02 20:09:04 +0100

    Tagit bort onödiga rader och kommentarer

--- matilda_project.ts
+++ matilda_project.ts
@@ -1,12 +1,10 @@
-import { lg_new, ListGraph, build_array } from "./lib/graphs";
-import { list, List, length } from "./lib/list";
-import { process_input } from "./engla_project";
-import { people_graph, person } from "./groupsort";
+import { lg_new, ListGraph } from "./lib/graphs";
+import { list } from "./lib/list";
+import { person } from "./groupsort";
 
 /**
- * 
+ * Makes a ListGraph from array of people
  * @param arr : array of type person
- * 
  */
 export function make_list_graph(arr: Array<person>): ListGraph {
     const len = arr.length;
@@ -16,16 +14,8 @@ export function make_list_graph(arr: Array<person>): ListGraph {
         return peoplegraph;
     } else {
         for (let i = 0; i < len; i = i + 1) { //ändra så att det börjar på 1 likt id's gör OM INTE annan idé
-            /**
-             * behövs det något sätt att veta om ens vän redan är inlagd? Kommer det att skapas hm. 
-             * ska noderna vara id's? isf, förstår denna funktion att den ska peka rätt?
-             * Ska noderna inte vara id's utan ett index krävs någon sök-algoritm för att veta om ens vän redan finns med för att peka
-             * på rätt index. 
-             */
             peoplegraph.adj[arr[i].id] = list(arr[i].friend_id!);
         }
     }
     return peoplegraph;
 }
-
-
