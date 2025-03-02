bc74c4655f807808b84c4e95b94ddab9f20075a5
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-02 20:40:15 +0100

    fler tester

--- testfile.test.ts
+++ testfile.test.ts
@@ -26,3 +26,23 @@ const ngrps2 = divide_if_too_many(grps2, grn, mgs);
 test('funkar det när den behöver köra ett varv till?', () => {
     expect(ngrps2).toStrictEqual(engrps2);
 });
+
+const grps3 = [[1,2,3], [4,5], [6,7,8], [9], [10,11,12], [13]]; //dock detta exemplet orimligt för så många får inte vara anmälda!!
+const engrps3 = [[1,2,3], [4,5,9], [6,7,8], [10,11,12]];
+
+const ngrps3 = divide_if_too_many(grps3, grn, mgs);
+
+test('vad händer när grupperna är för stora?', () => {
+    expect(engrps3).toStrictEqual(ngrps3);
+
+}); // då skippar den en grupp eftersom det är så den är byggd HOPPSAN
+
+const grps4 = [[1,2], [3,12], [4,5], [6,7], [8,9], [10,11]];
+const engrps4 = [[1,2,3]];
+const ngrps4 = divide_if_too_many(grps4, grn, mgs);
+
+test('vad händer om alla grupper är lika stora?', () => {
+    expect(engrps4).toStrictEqual(ngrps4);
+});
+//den tar isåfall BORT en grupp i taget tills det är rätt mängs grupper, frågan är vilka input som egentligen blir relevanta för dethär?
+//alltså: Kommer den behöva hantera inputs där grupperna blir som i testet ovan. 
