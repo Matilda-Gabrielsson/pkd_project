325070fc802149c925cfde990c335b09fcfda734
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-02 20:40:00 +0100

    inga egentliga ändringar

--- divide_if_too_many.ts
+++ divide_if_too_many.ts
@@ -13,6 +13,7 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
         const smallest_group = find_small.group; //the smallest group
         const smallest_group_index: number = find_small.index; //the index of the smallest group
         let groups_new = new Array<Array<number>>(groups.length-1); //gör att det blir en set size på nya arrayen så att den förkortas per körning
+        //obs detta gör att den klipper bort grupper om de är för stora LOL
 
         for(let i = 0; i < smallest_group_index; i = i + 1) {
             groups_new[i] = groups[i];
@@ -27,8 +28,11 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
                 //Tänker typ med grupper på en person känns det rimligt
                 groups_new[i] = [...groups[i], ...smallest_group];
                 break;
-            }   
+            } else {
+                //LÄR ju behöva vara något mer här
+            }
         }
+        console.log(groups_new);
         return(divide_if_too_many(groups_new, number_of_groups, max_group_size));
     } else {
         return groups;
